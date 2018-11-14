75d8f5d13e6e461fa981b4d2de5cfb3688117d0c
Junya Sewai <jsewai@myseneca.ca>
2018-11-14 02:06:21 -0500

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  const profiles = [
+    {name: "junya", age: 10},
+    {name: "hanakooooo", age:123}
+  ]
+  return(
+    <div>
+      {
+        profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+
+    </div>
+  )
 }
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}</div>
+}
+
+
 
 export default App;
